78f047cb0fff293d7026d57fee425a66e77a1b9f
Jorge Perez <jlpg81@gmail.com>
2023-11-10 12:01:21 +0100

    changed style and readme

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # radio-app
 
+How to run:
+Clone
+In terminal: flutter pub get
+Open emulator and press ctrl+f5.
+
+Works with android emulators, not for iOS.
+
 Backend API:
 Bando Radio API
 
--- lib/src/home/country_selector.dart
+++ lib/src/home/country_selector.dart
@@ -28,13 +28,17 @@ class _CountrySelectorState extends State<CountrySelector> {
     var radioList = countryList.map<DropdownMenuItem>((value) {
       return DropdownMenuItem<String>(
         value: value['countrycode'],
-        child: Text(value['country']),
+        child: Text(
+          value['country'],
+          style: TextStyle(color: Colors.orange),
+        ),
       );
     }).toList();
 
     return DropdownButton(
       value: initialValue,
       items: radioList,
+      dropdownColor: Colors.grey[800],
       onChanged: (value) => funcc(value),
     );
   }
